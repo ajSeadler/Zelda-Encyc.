de442e66f1a8bf729e8412e2bff9f875624c8342
ajSeadler <aj6287@gmail.com>
2023-10-01 19:15:31 -0500

    TRIFORCE ADD

--- zelda/src/App.jsx
+++ zelda/src/App.jsx
@@ -1,33 +1,41 @@
-// src/App.js
-
-import React from 'react';
-import { Route, Routes, Link, useNavigate } from 'react-router-dom';
-import GameList from './components/GamesList';
-import GameDetailPage from './components/GameDetailsPage';
-import "./index.css"
-import "./app.css"
+import React from "react";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import GameList from "./components/GamesList";
+import GameDetailPage from "./components/GameDetailsPage";
+import "./index.css";
+import "./app.css";
 
 function HomePage() {
   const navigate = useNavigate(); // Get the navigate function
 
   const handleShowGames = () => {
     // Redirect to the games page when the button is clicked
-    navigate('/games');
+    navigate("/games");
   };
 
   return (
-    <div>
-      <h1>The Legend of Zelda</h1>
-      <button className="show-games-button" onClick={handleShowGames}>
-  <span>Show Games</span>
-</button>
-<button className="show-games-button-1" onClick={handleShowGames}>
-  <span>Characters</span>
-</button>
-<button className="show-games-button-2" onClick={handleShowGames}>
-  <span>Dungeons</span>
-</button>
+    <>
+  <h1>THE LEGEND OF ZELDA FANCYCLOPEDIA</h1>
+
+    <div className="center-container"> {/* Wrap everything in a centering container */}
+      <div id="triforce">
+        <div id="triangle" className="shadow">
+          <div id="shadow"></div>
+        </div>
+      </div>
+      <div className="button-container">
+    <button className="show-games-button" onClick={handleShowGames}>
+      <span>Games</span>
+    </button>
+    <button className="show-games-button-1" onClick={handleShowGames}>
+      <span>Characters</span>
+    </button>
+    <button className="show-games-button-2" onClick={handleShowGames}>
+      <span>Dungeons</span>
+    </button>
   </div>
+    </div>
+    </>
   );
 }
 
